ffdaeeaf15e31692923e537f60bbd735a400f38b
tataqt <tata_98@ukr.net>
2021-06-15 18:06:53 +0300

    lesson 1 bonus

--- 01-cards/app.js
+++ 01-cards/app.js
@@ -1,15 +1,22 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 2) {
+    const slides = document.querySelectorAll('.slide');
 
+    console.log(activeSlide);
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClassess();
-        slide.classList.add('active');
-    });
+    slides[activeSlide].classList.add('active');
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClassess();
+            slide.classList.add('active');
+        });
+    }
+
+    function clearActiveClassess() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    }
 }
 
-function clearActiveClassess() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    });
-}
+slidesPlugin(Math.floor(Math.random() * 5));
