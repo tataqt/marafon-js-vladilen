fd97f18a55f5d410a76fd17210d456339e1a2d25
tataqt <tata_98@ukr.net>
2021-06-17 00:15:39 +0300

    third lesson complete

---
+++ 03-image-slider/app.js
@@ -0,0 +1,36 @@
+const downBtn = document.querySelector('.down-button');
+const upBtn = document.querySelector('.up-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
+const container = document.querySelector('.container');
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount -1)*100}vh`;
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+});
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    const height = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex*height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex*height}px)`;
+};
